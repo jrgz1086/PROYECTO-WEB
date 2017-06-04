508f761b805128310f016e850d5413759c1d8c81
Jorge <jorge.gonzalez1086@gmail.com>
2017-06-03 22:57:46 -0400

    Se implementó una pantalla con los campos "destino" "asunto" y "mensaje" para el envio de e-mail

--- src/com/mail/MailSender.java
+++ src/com/mail/MailSender.java
@@ -2,7 +2,6 @@ package com.mail;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.mail.Address;
@@ -15,14 +14,20 @@ import javax.mail.internet.MimeMessage;
 
 @ManagedBean(name = "mailSender")
 public class MailSender implements Serializable {
+	
+	private static final long serialVersionUID = -8780269596203245866L;
+
 	@Resource(mappedName = "java:jboss/mail/Gmail")
 	private Session session;
+	
+	private String correoDestino;
+	private String asunto;
+	private String mensaje;
 
 	public String sendMailTest() throws MessagingException {
 		System.out.println("Eviando... ");
 		try {
-			sendMail("jorge.gonzalez1086@gmail.com", "jorge.gonzalez@pti.org.py", "mensaje",
-					"Mensaje de Prueba");
+			sendMail("jorge.gonzalez1086@gmail.com", correoDestino, asunto, mensaje);
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
@@ -33,13 +38,36 @@ public class MailSender implements Serializable {
 			String sMailText) throws MessagingException {
 		MimeMessage message = new MimeMessage(session);
 		message.setFrom(new InternetAddress(mailFrom));
-		message.setReplyTo(new Address[] { new InternetAddress(
-				"jorge.gonzalez1086@gmail.com") });
+		message.setReplyTo(new Address[] { new InternetAddress(mailFrom) });
 		message.addRecipient(Message.RecipientType.TO, new InternetAddress(
 				sMailTo));
 		message.setHeader("Content-Type", "Contenido BODY del mail");
 		message.setSubject(sSubject);
 		message.setText(sMailText);
 		Transport.send(message);
+	}
+
+	public String getCorreoDestino() {
+		return correoDestino;
+	}
+
+	public void setCorreoDestino(String correoDestino) {
+		this.correoDestino = correoDestino;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
 	}
 }
